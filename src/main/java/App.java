4c3318c7c358682ab50c358d6406f11b38ef55f4
vaku <vaku878@gmail.com>
2023-03-10 20:29:49 +0400

    jdbc & sys_env added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,10 @@
+import java.sql.*;
 public class App {
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERNAME = System.getenv("LS_PG_USERNAME");
+    private static final String PASSWORD = System.getenv("LS_PG_PASSWORD");
+
+    public static void main(String[] args) {
+
+    }
 }
