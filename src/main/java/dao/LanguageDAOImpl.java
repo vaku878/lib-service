42d2e544e60abec2fd645cc16c1a872e8cfd7037
vaku <vaku878@gmail.com>
2023-03-12 23:28:46 +0400

    LanguageDAOImpl added

---
+++ src/main/java/dao/LanguageDAOImpl.java
@@ -0,0 +1,25 @@
+package dao;
+
+import dto.Language;
+
+public class LanguageDAOImpl implements LanguageDAO{
+    @Override
+    public int create(Language language) {
+        return 0;
+    }
+
+    @Override
+    public Language read(int id) {
+        return null;
+    }
+
+    @Override
+    public int update(Language language) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Language language) {
+        return 0;
+    }
+}
