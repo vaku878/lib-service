6669c2743ad2edcb9369695dfefb14baeff9c3cd
vaku <vaku878@gmail.com>
2023-03-12 23:25:46 +0400

    Language DTO implemented

--- src/main/java/dto/Language.java
+++ src/main/java/dto/Language.java
@@ -1,4 +1,33 @@
 package dto;
 
+import java.util.UUID;
+
 public class Language {
+    private UUID id;
+    private String name;
+
+    public Language() {
+
+    }
+
+    public Language(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
